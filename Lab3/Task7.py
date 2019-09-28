c67670f4fd99dd7dc5294bf343597ec8e2651bb1
Libo-Xu <55094824+Libo-Xu@users.noreply.github.com>
2019-09-28 20:33:17 +0200

    Update Task7.py

--- Lab3/Task7.py
+++ Lab3/Task7.py
@@ -11,8 +11,8 @@ Mask = gen_list(path, 'Mask')
 Img = gen_list(path,'Image')
 
 Mask_train, Mask_val, Img_train, Img_val = shuffle_split(Mask, Img, 0.8)
-Mask_train = read_mask(path+'Mask/', Mask_train, img_h, img_w)
-Mask_val =read_mask(path+'Mask/', Mask_val, img_h, img_w)
+Mask_train = read_data(path+'Mask/', Mask_train, img_h, img_w)
+Mask_val =read_data(path+'Mask/', Mask_val, img_h, img_w)
 Img_train = read_data(path+'Image/', Img_train, img_h, img_w)
 Img_val = read_data(path+'Image/', Img_val, img_h, img_w)
 
